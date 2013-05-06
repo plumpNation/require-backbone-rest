7c60b9e6f24c9b4241b63048d997d165a7d44f8f
Gavin King <gavin.king@fronter.com>
2013-05-06 17:48:43 +0200

    [Working with precompiled mustache partials]

--- app/scripts/views/books/list.js
+++ app/scripts/views/books/list.js
@@ -3,28 +3,28 @@ define([
     'mustache',
     'backbone',
     'collections/books',
-    'text!templates/books/list.mustache'
-], function (Mustache, Backbone, BooksCollection, Template) {
+    'text!templates/books/list.mustache',
+    'text!templates/books/view.mustache',
+], function (Mustache, Backbone, BooksCollection, BookListTemplate, BookViewTemplate) {
     'use strict';
 
     var BookListView = Backbone.View.extend({
 
         el: $('#content-container'),
 
-        template: Mustache.compile(Template),
-
         initialize: function () {
             var compiledTemplate;
+
+            Mustache.compilePartial('bookView', BookViewTemplate);
+            this.booklistTemplate = Mustache.compile(BookListTemplate);
 
             this.collection = new BooksCollection();
             this.collection.add({id: 2, title: "Something"});
 
             console.log(this.collection.models);
 
-            // Compile the template using Underscores micro-templating
-            compiledTemplate = this.template({'books': this.collection.models});
-
-            console.log(compiledTemplate);
+            // Compile the booklistTemplate using Underscores micro-templating
+            compiledTemplate = this.booklistTemplate({'books': this.collection.models});
 
             this.$el.html(compiledTemplate);
         }
