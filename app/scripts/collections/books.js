c478ae87f51a360d023f03802460a68b4779b35a
Gavin King <gavin.king@fronter.com>
2013-05-10 00:24:54 +0200

    [Off in another direction - not working]

--- app/scripts/collections/books.js
+++ app/scripts/collections/books.js
@@ -6,12 +6,15 @@ define([
     'use strict';
 
     var Books = Backbone.Collection.extend({
+
         'model': BookModel,
+
         'url': Config.Books.Api,
+
         // Save all of the todo items under the `"todos"` namespace.
         // 'localStorage': new Backbone.LocalStorage('todos-backbone')
         initialize: function () {
-            console.log('initialize Book collection');
+            console.log('Initialising book collection');
         }
     });
 
--- app/scripts/views/books/list.js
+++ app/scripts/views/books/list.js
@@ -2,10 +2,11 @@
 define([
     'mustache',
     'backbone',
+    'views/books/view',
     'collections/books',
-    'text!templates/books/list.mustache',
     'text!templates/books/view.mustache',
-], function (Mustache, Backbone, BooksCollection, BookListTemplate, BookViewTemplate) {
+    'text!templates/books/list.mustache',
+], function (Mustache, Backbone, BookView, BooksCollection, BookViewTemplate, BookListTemplate) {
     'use strict';
 
     var BookListView = Backbone.View.extend({
@@ -15,21 +16,41 @@ define([
         booklistTemplate: Mustache.compile(BookListTemplate),
 
         initialize: function () {
-            _.bindAll(this, 'render');
-
-            Mustache.compilePartial('bookView', BookViewTemplate);
+            _.bindAll(this);
 
             this.collection = new BooksCollection();
-            this.collection.fetch().done(this.render);
+
+            this.collection.on('sync', this.addAll, this);
+            // Some books etc insist that this should be 'reset', however
+            // the changelog for v1.0.0 got me looking under the hood into the
+            // annotated source which is here:
+            // http://backbonejs.org/docs/backbone.html
+            // If the option {reset: true} is not passed into the fetch method
+            // the 'reset' event is not fired.
+            // Read http://backbonejs.org/#upgrading
+
+            this.collection.fetch();
+            this.render();
+        },
+
+        addOne: function (model) {
+            console.log(model);
+            var bookView = new BookView({
+                'model': model,
+                'el': this.$el
+            });
+        },
+
+        addAll: function () {
+            _(this.collection.models).each(this.addOne);
         },
 
         render: function () {
             var renderedTemplate;
 
-            console.info('rendering book list');
-            console.info(this.collection.models);
+            console.info('Rendering book list');
 
-            renderedTemplate = this.booklistTemplate({'books': this.collection.models});
+            renderedTemplate = this.booklistTemplate();
 
             this.$el.html(renderedTemplate);
         }
